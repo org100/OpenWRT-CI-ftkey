251592d10fa294f157dacd93c480716ab6303909
LaoT <ftkey@qq.com>
2024-11-21 13:49:19 +0800

    测试设置向导和design主题

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -22,7 +22,10 @@ UPDATE_PACKAGE() {
 
 #UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
+UPDATE_PACKAGE "design" "papagaye744/luci-theme-design" "js"
 # UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
+
+UPDATE_PACKAGE "luci-app-netwizard" "sirpdboy/luci-app-netwizard"
 
 UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
 # UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main"
